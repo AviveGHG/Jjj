038392d2b042c9691b5cc616aa4b9298e470e2fa
AviveGHG <jonasjanik2023@gmail.com>
2026-07-05 20:44:10 +0200

    Update Meinhard.lua

--- Meinhard.lua
+++ Meinhard.lua
@@ -153,7 +153,7 @@ UIS.InputChanged:Connect(function(input)
 end)
 
 local autoTP, flying = false, false
-local AUTO_POS = CFrame.new(470.08, 197.57, -824.11)
+local AUTO_POS = CFrame.new(1194.13, 195.37, -1142.86)
 local flySpeed, flySmooth, flyBV = 85, 0.18, nil
 
 local function btn(text, y)
